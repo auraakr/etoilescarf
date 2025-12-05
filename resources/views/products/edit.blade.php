09863731140688621801372b0f4732efc550280a
auraakr <aura232kireina@gmail.com>
2025-12-05 16:46:54 +0700

    edit ui button

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,15 +1,69 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Products') }}
+            {{ __('Edit Product') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h1>Form edit produk</h1>
-                <form action="{{ route('products.update', $product->id) }}" method="post">
+    <div class="py-5">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-5">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-sm p-3">
+                <form action="{{ route('products.update', $product->id) }}" method="POST" class="max-w-sm">
+                    @csrf
+                    @method('PUT')
+
+                    <!-- Nama Produk -->
+                    <div class="mb-5">
+                        <label for="name" class="block mb-2.5 text-sm font-medium text-heading">Nama Produk</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value="{{ $product->name }}"
+                            class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                            placeholder="Masukkan nama produk"
+                            required />
+                    </div>
+
+                    <!-- Harga -->
+                    <div class="mb-5">
+                        <label for="original_price" class="block mb-2.5 text-sm font-medium text-heading">Harga Produk</label>
+                        <input
+                            type="number"
+                            id="original_price"
+                            name="original_price"
+                            value="{{ $product->original_price }}"
+                            class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                            placeholder="Masukkan harga"
+                            required />
+                    </div>
+
+                    <!-- Kategori -->
+                    <div class="mb-5">
+                        <label for="id_category" class="block mb-2.5 text-sm font-medium text-heading">Kategori Produk</label>
+                        <select
+                            id="id_category"
+                            name="id_category"
+                            class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                            required>
+                            <option value="">Pilih Kategori</option>
+
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}"
+                                    {{ $product->id_category == $category->id ? 'selected' : '' }}>
+                                    {{ $category->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <button type="submit"
+                        class="text-body box-border border border-default-medium hover:bg-blue-600 hover:text-blue-50 focus:ring-3 focus:ring-blue-100 shadow-xs hover:font-medium rounded text-sm px-4 py-2.5 focus:outline-none">
+                        Update Produk
+                    </button>
+                </form>
+
+                <!-- <form action="{{ route('products.update', $product->id) }}" method="post">
                     @csrf
                     @method('PUT')
                     <div class="form-group">
@@ -35,7 +89,7 @@
                         </select>
                     </div>
                     <button type="submit" class="btn mt-3 btn-primary">Update product</button>
-                </form>
+                </form> -->
             </div>
         </div>
     </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,13 +3,12 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Products') }}
         </h2>
+        <a href="{{ route('products.create') }}" class="text-body box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium rounded text-sm px-4 py-2.5 focus:outline-none">Tambah produk</a>
     </x-slot>
 
     <div class="py-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-5">
-            <div class="p-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <a class="btn btn-sm btn-success" href={{ route('products.create') }}>Tambah Produk</a>
-
+            <div class="p-6 bg-white overflow-hidden shadow-sm sm:rounded-sm">
                 <x-table>
                     <x-slot:header>
                         <th class="px-6 py-3 font-medium">ID</th>
@@ -25,32 +24,29 @@
                             <td class="px-6 py-3">{{ $product->name }}</td>
                             <td class="px-6 py-3">{{ $product->original_price }}</td>
                             <td class="px-6 py-3">{{ $product->category->name }}</td>
+                            <td class="px-6 py-3">
+                                <div class="inline-flex rounded-base shadow-xs -space-x-px" role="group">
+                                    <a href="{{ route('products.edit', $product->id) }}" class="inline-flex items-center text-body bg-neutral-primary-soft border border-default hover:bg-orange-50 hover:text-orange-600 focus:ring-3 focus:ring-orange-100 font-medium leading-5 rounded-s-base text-sm px-3 py-2 focus:outline-none">
+                                        <svg class="w-4 h-4 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
+                                        </svg>
+                                        Edit
+                                    </a>
+                                    <form action="{{ route('products.destroy', $product->id) }}" method="post" class="inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="inline-flex items-center text-body bg-neutral-primary-soft border border-default hover:bg-red-50 hover:text-red-600 focus:ring-3 focus:ring-red-100 font-medium leading-5 rounded-e-base text-sm px-3 py-2 focus:outline-none">
+                                            <svg class="w-4 h-4 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+                                            </svg>
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
                         </tr>
                     @endforeach
                 </x-table>
-                <!-- <table class="min-w-full divide-y divide-gray-200">
-                    <tr>
-                        <th class="px-6 py-3 bg-gray-0 text-left text-xs font0medium text-gray-500 uppercase tracking-wider">Nama</th>
-                        <th class="px-6 py-3 bg-gray-0 text-left text-xs font0medium text-gray-500 uppercase tracking-wider">Harga</th>
-                        <th class="px-6 py-3 bg-gray-0 text-left text-xs font0medium text-gray-500 uppercase tracking-wider">Category</th>
-                        <th class="px-6 py-3 bg-gray-0 text-left text-xs font0medium text-gray-500 uppercase tracking-wider">Action</th>
-                    </tr>
-                    @foreach ( $products as $product )
-                    <tr>
-                        <td class="px-6 py-3 whitespace-nowrap">{{ $product->name }}</td>
-                        <td class="px-6 py-3 whitespace-nowrap">{{ $product->original_price }}</td>
-                        <td class="px-6 py-3 whitespace-nowrap">{{ $product->category->name ?? '-' }}</td>
-                        <td>
-                            <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary btn-sm">Edit</a>
-                            <form action="{{ route('products.destroy', $product->id) }}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                            </form>
-                        </td>
-                    </tr> 
-                    @endforeach
-                </table> -->
             </div>
         </div>
     </div>
